1e0afe69fd3e20493645af3bcd3e3b9404b5ff25
09singh <singhsanket745@gmail.com>
2026-02-13 02:39:00 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { EmployeeProvider } from "@/app/context/page";
+import { EmployeeProvider } from "@/context/page";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
